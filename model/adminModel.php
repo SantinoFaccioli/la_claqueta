66789049723561539599a3dc69bc621fc659211a
santino faccioli <santino.faccioli@gmail.com>
2026-05-18 17:23:34 -0300

    auto-deploy

--- model/adminModel.php
+++ model/adminModel.php
@@ -3,11 +3,11 @@ require_once 'model/dbConnect.php';
 
 class AdminModel extends dbConnect {
 
-    public function getUsuarioEmail($email){
+    public function getUsuarioEmail($nombre_usuario){
 
-        $query-> $this ->db->prepare('SELECT * FROM usuarios WHERE email = ?');
-        $query -> execute([$email]);
-        $usuario = $query->(PDO::FETCH_OBJ);
+        $query-> $this ->db->prepare('SELECT * FROM usuarios WHERE nombre_usuario= ?');
+        $query -> execute([$nombre_usuario]);
+        $usuario = $query-> (PDO::FETCH_OBJ);
 
     }
 
